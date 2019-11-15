119602374866862c3e5ebf2f6811c991055d4c7e
Doshi <doshi@Doshis-MacBook-Pro.local>
2019-11-14 23:28:56 -0500

    glfw init and window creation

--- main.cpp
+++ main.cpp
@@ -1,73 +1,40 @@
 #include <iostream>
+#include <stdlib.h>
 
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
 
-// GLFW
+//GLFW
 #include <GLFW/glfw3.h>
 
-// Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
 // The MAIN function, from here we start the application and run the game loop
-int main( )
-{
-    // Init GLFW
-    glfwInit( );
+int main( ){
+
+//  This function initializes the GLFW library.  Before most GLFW functions can 
+//  be used, GLFW must be initialized, and before an application terminates GLFW 
+//  should be terminated in order to free any resources allocated during or after initialization.
+    glfwInit();
     
-    // Set all the required options for GLFW
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
-    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
-    
-    // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr );
-    
-    int screenWidth, screenHeight;
-    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
-    
-    if ( nullptr == window )
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate( );
-        
+
+
+    // GLFW window creation
+    /*
+        The window handle is passed to all window related functions and is provided 
+        to along with all input events, so event handlers can tell which window received the event.
+    */
+    glfwGetVideoMode *mode = glfwGetVideoMode(monitor);
+
+    GLFW *window = glfwCreateWindow(WIDTH, HEIGHT, "Call of Duty Modern Warfare", glfwGetPrimaryMonitor(), nullptr);
+
+    // glfwCreateWindow could returns NULL, so check
+    if(nullptr == window){
+        glfwDestroyWindow(window);
         return EXIT_FAILURE;
     }
-    
-    glfwMakeContextCurrent( window );
-    
-    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
-    glewExperimental = GL_TRUE;
-    // Initialize GLEW to setup the OpenGL Function pointers
-    if ( GLEW_OK != glewInit( ) )
-    {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        return EXIT_FAILURE;
-    }
-    
-    // Define the viewport dimensions
-    glViewport( 0, 0, screenWidth, screenHeight );
-    
-    // Game loop
-    while ( !glfwWindowShouldClose( window ) )
-    {
-        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-        glfwPollEvents( );
-        
-        // Render
-        // Clear the colorbuffer
-        glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
-        glClear( GL_COLOR_BUFFER_BIT );
-        
-        // Swap the screen buffers
-        glfwSwapBuffers( window );
-    }
-    
-    // Terminate GLFW, clearing any resources allocated by GLFW.
-    glfwTerminate( );
-    
-    return EXIT_SUCCESS;
+
+
+
 }
